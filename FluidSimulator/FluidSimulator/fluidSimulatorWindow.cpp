11332e55b090b5461ca39f01a0ce1055dc38743b
Leonardo Ciurcau <leonardo.ciurcau@stud.ubbcluj.ro>
2023-12-27 16:07:09 +0200

    Resized box

--- FluidSimulator/FluidSimulator/fluidSimulatorWindow.cpp
+++ FluidSimulator/FluidSimulator/fluidSimulatorWindow.cpp
@@ -6,11 +6,15 @@ void FluidSimulatorWindow::draw(ImGuiIO& io)
 
     ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
 
-    ImVec2 rectPos(50, 50);
+    ImVec2 rectPos(50, ImGui::GetFontSize() * 10);
     ImVec2 rectSize(ImGui::GetWindowWidth() - rectPos.x * 2, ImGui::GetWindowHeight() - rectPos.y - ImGui::GetFontSize());
     ImU32 rectColor = IM_COL32(155, 155, 155, 255);
 
     ImGui::GetWindowDrawList()->AddRect(rectPos, ImVec2(rectPos.x + rectSize.x, rectPos.y + rectSize.y), rectColor);
+
+    ImGuiStyle& style = ImGui::GetStyle();
+    ImU32 bgColor = ImGui::GetColorU32(style.Colors[ImGuiCol_WindowBg]);
+    ImGui::GetWindowDrawList()->AddLine(rectPos, {rectPos.x + rectSize.x, rectPos.y}, bgColor);
 
     ImGui::End();
 }
